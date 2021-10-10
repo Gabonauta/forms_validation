fde0a86c6c10860065a2dfd800b5698f444bafa9
Gabonauta <dante9825@gmail.com>
2021-10-10 14:16:29 -0500

    Initial project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:forms_validation/screens/screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Productos',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home_page',
+      routes: {
+        'login': (_) => LoginScreen(),
+        'home': (_) => HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:forms_validation/screens/home_screen.dart';
+export 'package:forms_validation/screens/login_screen.dart';
